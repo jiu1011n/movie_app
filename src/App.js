6ea428ac12bfac7b8ef680ced72a9b2276f184f2
jiu <jiu1011n@gmail.com>
2022-12-09 14:58:54 +0900

    fix code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ movie, setMovie ] = useState();
+
+  useEffect(() => {
+    fetch('https://api.themoviedb.org/3/movie/{movie_id}?api_key=047e66f105b553e0beff7f005bd621d0&language=ko&append_to_response=KR')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("data")
+      })
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
   );
 }
---
+++ src/components/movie.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Movie(){
+    
+    return(<div>
+        hi
+    </div>)
+}
